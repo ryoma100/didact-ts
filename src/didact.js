4c68a40f5d380a285e091385548126daa0ce295e
Ryouichi Matsuda <ryouichi.matsuda@nifty.ne.jp>
2023-10-11 21:22:09 +0900

    add react-scripts

--- src/didact.js
+++ src/didact.js
@@ -299,21 +299,8 @@ function reconcileChildren(wipFiber, elements) {
   }
 }
 
-const Didact = {
+export const Didact = {
   createElement,
   render,
   useState,
 }
-
-/** @jsx Didact.createElement */
-function Counter() {
-  const [state, setState] = Didact.useState(1)
-  return (
-    <h1 onClick={() => setState(c => c + 1)}>
-      Count: {state}
-    </h1>
-  )
-}
-const element = <Counter />
-const container = document.getElementById("root")
-Didact.render(element, container)
